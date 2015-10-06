989cf8f92aeb2af46ec7fdef5f900748fb44ee04
Robert Foy <robert.foy@ventyx.abb.com>
2015-10-06 12:14:14 +1000

    Tidy up code

--- storyboarder.user.js
+++ storyboarder.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name          StoryBoarder
-// @description	  Add colour codes to V1 for Release Notes
+// @description	  Enhances V1 for Ventyx & Ellipse
 // @include       https://www11.v1host.com/VentyxProd/*
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js
 // @require       https://gist.githubusercontent.com/BrockA/2625891/raw/fd02ec05e3079cdd52cf5892a7ba27b67b6b6131/waitForKeyElements.js
 // @author        rf5860
-// @version       0.7
+// @version       0.8
 // @updateURL     https://github.com/rf5860/Storyboard-Prettifier/raw/master/storyboarder.user.js
 // @downloadURL   https://github.com/rf5860/Storyboard-Prettifier/raw/master/storyboarder.user.js
 // ==/UserScript==
@@ -24,11 +24,15 @@ var ACCEPTED = 'Accepted';
 var COMPLETED = 'Completed';
 var DEV_TASK_TEXT = 'Development';
 var TASKS_URL = "https://www11.v1host.com/VentyxProd/rest-1.v1/Data/Task?sel=Status.Name&find=" + DEV_TASK_TEXT + "&where=Parent={0}&Accept=application/json";
+var SFDC_URL = "http://litsfvm1/sfcaselookup/default.aspx?Case='{0}'";
+var SFDC_HTML = '<a href="' + SFDC_URL + '">{0}</a>');
+var GITHUB_URL = "https://github.com/Mincom/ellipse/commit/{0}";
+var GITHUB_HTML = '<a href="' + GITHUB_URL + '">{0}</a>';
 
 function addGitHubLinks(c) {
     c.each(function(i, l) {
         v = $(l);
-        v.html('<a href="https://github.com/Mincom/ellipse/commit/' + v.text() + '">' + v.text() + "</a>");
+        v.html(GITHUB_HTML.replace("{0}", v.text()));
     });
 }
 
@@ -36,7 +40,7 @@ function addSFDCLinks(s) {
     s.each(function(i, l) {
         var targetDiv = $(l).parent().find('div.value');
         var caseNumber = targetDiv.html();
-        targetDiv.html('<a href="http://litsfvm1/sfcaselookup/default.aspx?Case=' + caseNumber + '">' + caseNumber + '</a>');
+        targetDiv.html(GITHUB_HTML.replace("{0}", caseNumber));
     });
 }
 
